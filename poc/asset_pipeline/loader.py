241bdc34770826bc5f0b37a88ebecc04fe9c1106
Axis <77634274+blankRiot96@users.noreply.github.com>
2023-12-16 15:57:50 +0530

    sound loader added

--- poc/asset_pipeline/loader.py
+++ poc/asset_pipeline/loader.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pygame
 
-from .common import IMAGES_PATH
+from .common import IMAGES_PATH, SOUND_PATH
 
 
 def _find_file_path(file_name: str, directory: Path, pattern: str) -> Path:
@@ -16,7 +16,8 @@ def _find_file_path(file_name: str, directory: Path, pattern: str) -> Path:
 
 @lru_cache
 def load_image(file_name: str) -> pygame.Surface:
-    """Loads an image from the disk and caches it.
+    """
+    Loads an image from the disk and caches it.
 
     Attributes
     ----------
@@ -24,7 +25,6 @@ def load_image(file_name: str) -> pygame.Surface:
     that recursively.
     """
 
-    print("SUSSY BACKKKAKAKA")
     file_path = _find_file_path(file_name, IMAGES_PATH, "*")
     raw_surface = pygame.image.load(file_path)
     alpha = raw_surface.get_alpha()
@@ -33,3 +33,18 @@ def load_image(file_name: str) -> pygame.Surface:
         return raw_surface.convert()
 
     return raw_surface.convert_alpha()
+
+
+@lru_cache
+def load_sound(file_name: str) -> pygame.mixer.Sound:
+    """
+    Loads a sound file from the disk and caches it.
+
+    Attributes
+    ----------
+    file_name: Represents the name of the file. No need for the exact path. Searches
+    that recursively.
+    """
+
+    file_path = _find_file_path(file_name, SOUND_PATH, "*")
+    return pygame.mixer.Sound(file_path)
